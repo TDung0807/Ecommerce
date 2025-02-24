927413f5c3bf13828062c48b063c7b24bf04439c
TrDung <52100783@student.tdtu.edu.vn>
2025-02-24 11:33:16 +0700

    CRUD user

--- ecom/app/controllers/api/v1/users_controller.rb
+++ ecom/app/controllers/api/v1/users_controller.rb
@@ -1,7 +1,21 @@
 module Api
   module V1
     class UsersController < ApplicationController
-      skip_before_action :authenticate_user!, only: [:create] 
+      before_action :set_user, only: [:show, :update, :destroy]
+      skip_before_action :authenticate_user!, only: [:create]
+
+      # GET /api/v1/users
+      def index
+        users = User.all
+        render json: users
+      end
+
+      # GET /api/v1/users/:id
+      def show
+        render json: @user
+      end
+
+      # POST /api/v1/users
       def create
         user = User.new(user_params)
         if user.save
@@ -10,9 +24,29 @@ module Api
           render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
         end
       end
-    
+
+      # PATCH/PUT /api/v1/users/:id
+      def update
+        if @user.update(user_params)
+          render json: { message: "User updated successfully", user: @user }
+        else
+          render json: { errors: @user.errors.full_messages }, status: :unprocessable_entity
+        end
+      end
+
+      # DELETE /api/v1/users/:id
+      def destroy
+        @user.destroy
+        render json: { message: "User deleted successfully" }, status: :ok
+      end
+
       private
-    
+      def set_user
+        @user = User.find(params[:id])
+      rescue ActiveRecord::RecordNotFound
+        render json: { error: "User not found" }, status: :not_found
+      end
+
       def user_params
         params.require(:user).permit(:email, :username, :password, :fullname, :location, :birth)
       end
